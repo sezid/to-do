5bf23a6b4aa013977b5073d0b8733d117bafb92b
sezid <bradsajid@gmail.com>
2024-09-12 15:23:20 +0600

    created add field

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import TableHeader from './Components/TableHeader/TableHeader'
 import Content from './Components/Content/Content'
+import Create from './Components/Create/Create'
 import './App.css'
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
       <>
       <TableHeader/>
       <Content/>
+      <Create/>
       </>
     )
     
---
+++ src/Components/Create/Create.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import './Create.css'
+
+const Create = () => {
+  return (
+    <div className='add-field'>
+
+        <ul>
+            <li><input type="text" /></li>
+            <li><button>+</button></li>
+        </ul>
+
+
+      
+    </div>
+  )
+}
+import './Create.css'
+
+export default Create
